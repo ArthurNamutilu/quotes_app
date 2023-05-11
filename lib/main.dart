bc2b49f659cbbeea930b6a7275f3bf2ce77359c9
ArthurNamutilu <arthurggnamutilu@gmail.com>
2023-05-11 21:58:28 +0300

    list of data

--- lib/main.dart
+++ lib/main.dart
@@ -1,2 +1,31 @@
 import 'package:flutter/material.dart';
-void main() => runApp();
+void main() => runApp(const MaterialApp(
+  home: NiceQuotes(),
+));
+class NiceQuotes extends StatefulWidget {
+  const NiceQuotes({Key? key}) : super(key: key);
+
+  @override
+  State<NiceQuotes> createState() => _NiceQuotesState();
+}
+
+class _NiceQuotesState extends State<NiceQuotes> {
+  List<String> quotes = [
+    'Procrastination is the thief of time',
+    'Laziness is a secret ingredient that goes into failure',
+    'Laziness is the mother of all bad habits'
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.amberAccent,
+      appBar: AppBar(
+        title: const Text('Awesome quotes'),
+        backgroundColor: Colors.redAccent,
+      ),
+      body: Column(
+        children: quotes.map((quote) => Text(quote)).toList(),
+      ),
+    );
+  }
+}
