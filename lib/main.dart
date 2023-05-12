d0b7a30255cf6e1492d7bc224e6f6bb54cedef82
ArthurNamutilu <arthurggnamutilu@gmail.com>
2023-05-12 07:02:54 +0300

    used quoteTemplate() function to list elements to a card

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,34 @@ class _NiceQuotesState extends State<NiceQuotes> {
     Quote(text: 'Laziness is a secret ingredient that goes into failure', author: 'Robert Half'),
     Quote(text: 'Laziness is the mother of all bad habits', author: 'Shikamaru Nara')
   ];
+  Widget quoteTemplate(quote){
+    return Card(
+      margin: const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0.0),
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              quote.text,
+              style: TextStyle(
+                fontSize: 18.0,
+                color: Colors.grey[600],
+              ),
+            ),
+            const SizedBox(height: 6.0,),
+            Text(
+              quote.author,
+              style: TextStyle(
+                fontSize: 14.0,
+                color: Colors.grey[800],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,7 +53,7 @@ class _NiceQuotesState extends State<NiceQuotes> {
         backgroundColor: Colors.redAccent,
       ),
       body: Column(
-        children: quotes.map((quote) => Text('${quote.text} - ${quote.author}')).toList(),
+        children: quotes.map((quote) => quoteTemplate(quote)).toList(),
       ),
     );
   }
